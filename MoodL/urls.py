eaf19b94ff2df7d09b66c2604a5b079472016752
AgusRaharja69 <59613324+AgusRaharja69@users.noreply.github.com>
2020-01-20 04:18:37 +0700

    Add files via upload

---
+++ MoodL/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from survey.views import logout_view, survey
+
+urlpatterns = [    
+    path('admin/', admin.site.urls),
+    path('', include('survey.urls')),
+    path('survey/', survey, name='survey'),
+    path('logout/', logout_view, name='logout'),
+
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
