b2693d7671e8cf129d27e48b0a9fa3dbc1c5ae18
AgusRaharja69 <59613324+AgusRaharja69@users.noreply.github.com>
2020-01-31 16:55:58 +0700

    Update urls.py

--- survey/urls.py
+++ survey/urls.py
@@ -6,7 +6,7 @@ app_name = 'survey'
 urlpatterns = [
 	
 	path('', views.main_base_view, name='main_base'),
-	path('survey/(?P<link1>[0-9]{3})/', views.survey, name='survey'),
+	path('survey/(?P<link1>[0-9]{6})/(?P<date_link>/', views.survey, name='survey'),
 	path('login/', views.login, name='login'),
 	path('logout/', views.logout_view, name='logout')
 
