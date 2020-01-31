05869ecef038dd9a9f03349b3df51d1af4a4748d
AgusRaharja69 <59613324+AgusRaharja69@users.noreply.github.com>
2020-01-31 07:24:45 +0700

    Update views.py

--- survey/views.py
+++ survey/views.py
@@ -5,27 +5,31 @@ from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-
+from .models import koresponden, link, subwil
+import datetime
 
 def main_base_view(request):
     dictionary = dict(request=request)
-    dictionary.update(csrf(request))
+    dictionary.update(csrf(request))    
     return render_to_response('main/index.html', dictionary)
 
-def survey(request):    
-    return render(request, 'main/survey.html')
+def survey(request, link1):
+    wilayah = subwil.objects.filter(kode=link1)[0]
+    return render(request, 'main/survey.html',{'wilayah': wilayah})
 
 
-def login(request):
+def login(request):        
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')   
+        link1 = request.POST.get('code1')     
+        user = authenticate(username=username, password=password)        
         if user is not None:
             auth_login(request=request, user=user)
             dictionary = dict(request=request)
             dictionary.update(csrf(request))
-            return redirect('/survey/')
+            link.objects.create(username=username,code=link1,date=datetime.datetime.now().strftime ("%Y-%m-%d"))
+            return redirect('survey',link1)
         else:
             msg_to_html = custom_message('Invalid Credentials', TagType.danger)
             dictionary = dict(request=request, messages = msg_to_html)
