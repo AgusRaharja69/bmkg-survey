c283a6472acea8b97e532756846b1c3ec034c094
AgusRaharja69 <59613324+AgusRaharja69@users.noreply.github.com>
2020-01-31 07:30:03 +0700

    Update urls.py

--- MoodL/urls.py
+++ MoodL/urls.py
@@ -4,17 +4,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from survey.views import logout_view, survey
+from survey.views import logout_view, survey, login
 
 urlpatterns = [    
     path('admin/', admin.site.urls),
     path('', include('survey.urls')),
-    path('survey/', survey, name='survey'),
-    path('logout/', logout_view, name='logout'),
+    path('survey/(?P<link1>\d)/', survey, name='survey'),
+    path('login/', login, name='login'),
+    #path('logout/', logout_view, name='logout'),
 
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
