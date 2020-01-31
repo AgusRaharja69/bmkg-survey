e90a911cb92ad367932bebd368e23eb4bfeb6a64
AgusRaharja69 <59613324+AgusRaharja69@users.noreply.github.com>
2020-01-31 07:26:10 +0700

    Update models.py

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class koresponden(models.Model):  
+    name = models.CharField(max_length=100)
+    NIP = models.CharField(max_length=100)
+    sex = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+    study = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class link(models.Model):
+    username = models.CharField(max_length=100)
+    code = models.CharField(max_length=50)
+    date = models.DateTimeField('Event Date')
+
+    def __str__(self):
+        return self.code
+
+class subwil(models.Model):
+    kode = models.CharField(max_length=50)
+    wilayah = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.wilayah
+#class data(models.Model):
